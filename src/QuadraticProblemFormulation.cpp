6e5ebb646d2f23eeaf430fbeb5d3b99bac80f19f
Samuel Bachmann <samuel.bachmann@gmail.com>
2016-05-08 21:28:45 +0200

    cast to SparceMatrix

--- src/QuadraticProblemFormulation.cpp
+++ src/QuadraticProblemFormulation.cpp
@@ -70,7 +70,8 @@ bool QuadraticProblemFormulation::solve(
   OOQPEI_ASSERT_EQ(range_error, W.rows(), n, "Matrix W has wrong size.");
 
   Eigen::SparseMatrix<double, Eigen::RowMajor> Q_temp;
-  Q_temp = A.transpose() * S * A + W.toDenseMatrix().sparseView();
+  Q_temp = A.transpose() * S * A + (Eigen::SparseMatrix<double, Eigen::RowMajor>)W.toDenseMatrix().sparseView();
+
 
   VectorXd c_temp = -A.transpose() * S * b;
 
